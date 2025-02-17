37ed6a2c1c52363948bb6510d2bb3e1ceb9f6da2
Nicolas Molina <hi@nicobytes.com>
2025-02-17 11:01:56 -0500

    chore: create enviroments

--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Product } from '../models/product.model';
+import { environment } from '@env/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,7 @@ export class ProductService {
   private http = inject(HttpClient);
 
   getProducts(category_id?: string) {
-    const url = new URL(`https://api.escuelajs.co/api/v1/products`);
+    const url = new URL(`${environment.apiUrl}/api/v1/products`);
     if (category_id) {
       url.searchParams.set('categoryId', category_id);
     }
@@ -17,8 +18,7 @@ export class ProductService {
   }
 
   getOne(id: string) {
-    return this.http.get<Product>(
-      `https://api.escuelajs.co/api/v1/products/${id}`,
-    );
+    const url = `${environment.apiUrl}/api/v1/products/${id}`;
+    return this.http.get<Product>(url);
   }
 }
