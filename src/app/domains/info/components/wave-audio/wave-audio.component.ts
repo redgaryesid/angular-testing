b992fb3aab2a4fdd710d4c694d3eee52b0116536
Nicolas Molina <hi@nicobytes.com>
2025-02-18 11:56:54 -0500

    chore: signal queries

--- src/app/domains/info/components/wave-audio/wave-audio.component.ts
+++ src/app/domains/info/components/wave-audio/wave-audio.component.ts
@@ -1,10 +1,10 @@
 import {
   Component,
   ElementRef,
-  ViewChild,
   signal,
   AfterViewInit,
-  input
+  input,
+  viewChild,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -17,14 +17,14 @@ import WaveSurfer from 'wavesurfer.js';
 })
 export class WaveAudioComponent implements AfterViewInit {
   readonly audioUrl = input.required<string>();
-  @ViewChild('wave') container!: ElementRef;
+  $waveContainerRef = viewChild.required<ElementRef<HTMLDivElement>>('wave');
   private ws!: WaveSurfer;
   isPlaying = signal(false);
 
   ngAfterViewInit() {
     this.ws = WaveSurfer.create({
       url: this.audioUrl(),
-      container: this.container.nativeElement,
+      container: this.$waveContainerRef().nativeElement,
     });
     this.ws.on('play', () => this.isPlaying.set(true));
     this.ws.on('pause', () => this.isPlaying.set(false));
