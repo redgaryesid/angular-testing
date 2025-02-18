2a2b4bb2eb2ea4af1e5aa894b7404c9f551edf7b
Nicolas Molina <hi@nicobytes.com>
2025-02-18 10:23:15 -0500

    chore: toSignal

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { toSignal } from '@angular/core/rxjs-interop';
 import { CounterComponent } from '@shared/components/counter/counter.component';
 import { HighlightDirective } from '@shared/directives/highlight.directive';
 
 import { WaveAudioComponent } from '@info/components/wave-audio/wave-audio.component';
 import { FormsModule } from '@angular/forms';
+import { BehaviorSubject, delay, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-about',
@@ -22,6 +23,16 @@ export default class AboutComponent {
   duration = signal(1000);
   message = signal('Hola');
 
+  obsWithInit$ = new BehaviorSubject<string>('init value');
+  $withInit = toSignal(this.obsWithInit$, {
+    requireSync: true,
+  });
+
+  obsWithoutInit$ = new Subject<string>();
+  $withoutInit = toSignal(this.obsWithoutInit$.pipe(delay(3000)), {
+    initialValue: '----',
+  });
+
   changeDuration(event: Event) {
     const input = event.target as HTMLInputElement;
     this.duration.set(input.valueAsNumber);
@@ -29,5 +40,13 @@ export default class AboutComponent {
 
   changeMessage(event: string) {
     console.log('changeMessage', event);
+  }
+
+  emitWithInit() {
+    this.obsWithInit$.next('new value');
+  }
+
+  emitWithoutInit() {
+    this.obsWithoutInit$.next('*****');
   }
 }
