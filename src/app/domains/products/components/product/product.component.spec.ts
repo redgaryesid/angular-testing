2b529b56f1eb0f363f9f5c0545508f8e07d6b884
redgaryesid <r.edgaryesid@gmail.com>
2025-04-28 23:20:27 -0500

    Clase 16, Pruebas de Componentes con Data Test ID y Selectores CSS

--- src/app/domains/products/components/product/product.component.spec.ts
+++ src/app/domains/products/components/product/product.component.spec.ts
@@ -1,21 +1,34 @@
-import { createRoutingFactory,Spectator } from "@ngneat/spectator/jest";
+import { byTestId, createRoutingFactory,Spectator } from "@ngneat/spectator/jest";
 
 import { ProductComponent } from "./product.component";
 import { generateFakeProduct } from "@shared/models/product.mock";
 
 describe("ProductComponent", () => {
+
     let spectator: Spectator<ProductComponent>;
+
     const createComponent = createRoutingFactory(ProductComponent);
     
+    const mockProduct = generateFakeProduct();
+
     beforeEach(() => {
         spectator = createComponent({
             detectChanges: false,
         });
-        spectator.setInput("product", generateFakeProduct());
+        spectator.setInput("product", mockProduct);
     });
     
     it("should create", () => {
         spectator.detectChanges();
         expect(spectator.component).toBeTruthy();
     });
+
+    it("should display product title", () => {
+        spectator.detectChanges();   
+        const element = spectator.query(byTestId('product-title'));
+        expect(element).toHaveText(
+            mockProduct.title
+        );
+    }
+    );
 });
